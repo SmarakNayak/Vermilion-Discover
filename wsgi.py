cbb3fb601d0db09a076f68bb7a9a62a89117f498
SmarakNayak <miltu.s.nayak@gmail.com>
2023-10-27 19:11:33 +1100

    gunicorn edit

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
-from main import app
+##Entrance for gunicorn
+from main import app, discover, model, index
+import threading
+
+index_thread = threading.Thread(target=discover.update_index, args=(index, model))
+index_thread.start()
+
 if __name__ == "__main__":
-   print("wsgi main hit")
-   app.run()
+    print("wsgi main hit")
+    app.run()
+    
